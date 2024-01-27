25982d8b017593f3122f7d42042d718eaf128842
Mustafa <105437960+Mustafayilmzz@users.noreply.github.com>
2024-01-27 04:24:46 +0300

    Update ft_putchar.c

--- 42_Piscine/rush00/ex00/ft_putchar.c
+++ 42_Piscine/rush00/ex00/ft_putchar.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putchar.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ahtuncay <ahtuncay@student.42kocaeli.com.  +#+  +:+       +#+        */
+/*   By: mustyilm <mustyilm@student.42kocaeli.com.  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/05 17:44:07 by ahtuncay          #+#    #+#             */
-/*   Updated: 2023/02/05 17:44:11 by ahtuncay         ###   ########.tr       */
+/*   Created: 2023/02/05 17:44:07 by mustyilm          #+#    #+#             */
+/*   Updated: 2023/02/05 17:44:11 by mustyilm         ###   ########.tr       */
 /*                                                                            */
 /* ************************************************************************** */
 
